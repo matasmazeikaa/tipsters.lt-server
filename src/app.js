4443d949a838ed61abcaf1309edeb24c7019f939
Matas Mažeika <matas.mazeika@192-168-10-54.local.balticum.lt>
2023-03-15 20:15:03 +0200

    add paysera payment link gen

--- src/app.js
+++ src/app.js
@@ -1,27 +1,47 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const Paysera = require("paysera-nodejs");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+const middlewares = require("./middlewares");
+const api = require("./api");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+function guidGenerator() {
+  return 100000 + Math.floor(Math.random() * 900000);
+}
+
+const paysera = new Paysera({
+  projectid: process.env.PAYSERA_PROJECT_ID,
+  sign_password: process.env.PAYSERA_PASSWORD,
+  accepturl: "https://tipsters.lt/mokejimas-pavyko",
+  cancelurl: "https://tipsters.lt/mokejimas-nepavyko",
+  callbackurl: "http://mycallback.url",
+  test: 1,
+});
+
+app.post("/pay", (req, res) => {
+  const { amount } = req.body;
+
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+    redirectUrl: paysera.buildRequestUrl({
+      orderid: guidGenerator(),
+      amount: Number(amount),
+      currency: "EUR",
+    }),
   });
 });
 
-app.use('/api/v1', api);
+app.use("/api/v1", api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
